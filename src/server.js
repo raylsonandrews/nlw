3b3e28b506f95f2da1a46c27f69728932fbd3b27
Raylson Andrews <andrewsraylson@gmail.com>
2020-08-09 14:23:41 -0300

    ajuste server e pagina study

--- src/server.js
+++ src/server.js
@@ -1,17 +1,82 @@
-const { response } = require('express')
+const proffys = [
+    { 
+    name: "Diego Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp: "85996652387", 
+    bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química", 
+    cost:"20", 
+    weekday: [0], 
+    time_from: [720], 
+    time_to: [1220] 
+    },
+    { 
+    name: "Daniela Evangelista", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    whatsapp: "85996652387", 
+    bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química", 
+    cost:"20", 
+    weekday: [1], 
+    time_from: [720], 
+    time_to: [1220]
+    },
+    { 
+        name: "Mayk Brito", 
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
+        whatsapp: "85996652387", 
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost:"20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+    }
 
-const express = require('express')
-const server = express()
+];
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+function pageLanding(require, response) {
+    return response.render("index.html")
+}
+
+function pageStudy(require, response) {
+    const filters = require.query;
+    return response.render("study.html", { proffys, filters, subjects })
+}
+
+function pageGiveClasses(require, response) {
+    return response.render("give-classes.html")
+}
+
+
+const { response } = require('express');
+
+const express = require('express');
+const server = express();
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
 server.use(express.static("public"))
 
-.get("/", (require, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (require, response) => {
-    return response.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (require, response) => {
-    return response.sendFile(__dirname + "/views/give-classes.html")
-})
-.listen(5500)
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+
+.listen(5500);
